02397d6dc7ed42f061a0ca696d8a57b7e3897a05
Michael <hubu99@gmail.com>
2025-10-15 13:58:33 -0300

    Initial commit

---
+++ GameProg_SuperVirusLand_MichaelT/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProg_SuperVirusLand_MichaelT
+{
+    internal class Program
+    {
+        static char[,] map = {
+            {'^','^','^','-','~','~','-','-','^','^'},
+            {'-','^','-','-','~','~','-','^','^','^'},
+            {'-','-','-','~','~','-','-','-','-','-'},
+            {'~','~','-','-','-','-','~','~','-','-'},
+            {'~','~','-','-','-','~','~','~','-','^'},
+            {'~','~','-','^','-','-','-','~','-','^'},
+            {'-','~','-','^','^','-','-','-','^','^'},
+            {'-','-','-','^','^','^','^','^','^','^'}
+        };
+
+        static void printMap() 
+        {
+            for (int y = 0; y < map.GetLength(0); y++) 
+            {
+                for (int x = 0; x < map.GetLength(1); x++) 
+                {
+                    char c = map[y, x];
+
+                    if (c == '-') Console.ForegroundColor = ConsoleColor.Green;
+                    else if (c == '~') Console.ForegroundColor = ConsoleColor.Blue;
+                    else if (c == '^') Console.ForegroundColor = ConsoleColor.Gray;
+                    else Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.Write(c);
+                }
+                Console.Write("\n");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            printMap();
+        }
+    }
+}
